428c758c1e16f9b3a56e35f58d3165e4a46d9ee9
Alexander Gigliotti Petruska <230888889+verslenvers@users.noreply.github.com>
2025-10-31 19:08:38 -0400

    BERT Architecture and Pretrained Weights in progress

---
+++ bert/bert.py
@@ -0,0 +1,92 @@
+import torch
+import re
+from torch import nn
+from encoder import Encoder
+from transformers import BertTokenizer
+from transformers import BertModel
+
+bert = BertModel.from_pretrained('bert-base-uncased')
+params = list(bert.named_parameters()) # needs to be converted into a list as it is an iterator, it would be exhausted in first run of the for loop otherwise.
+
+word_embeddings = bert.embeddings.word_embeddings
+positional_embeddings = bert.embeddings.position_embeddings
+segment_embeddings = bert.embeddings.token_type_embeddings
+trained_layer_norm = bert.embeddings.LayerNorm
+encoder_weights = bert.parameters
+batch_size = [16, 32]
+num_epochs = [2, 3, 4]
+learning_rate_adam = [5^(-5), 3^(-5), 2^(-5)]
+encoder_regex = "^encoder\.layer\."
+beta1=0.9
+beta2=0.999
+
+count = 0 
+layer_weights_dict = {}
+while count < 12:
+    layer_weights_sublist = []
+    regex_count = encoder_regex+str(count)+".*"
+    for name, param in params:
+        if re.match(regex_count, name):
+            layer_weights_sublist.append([name, param])
+            
+
+    layer_weights_dict[str(count)] = layer_weights_sublist
+    count += 1
+
+# print(layer_weights_dict.keys())
+
+
+# BERT (Bidirectional Encoder Representations from Transformers)
+class BERT(nn.Module):
+    def __init__(self, batch_size=2, seq_len=10):
+        super().__init__()
+        # the number of encoder blocks (BASE)
+        self.L = 12
+        
+        # Tokenizer & Embeddings
+        self.bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.word_embeddings = word_embeddings
+        self.positional_embeddings = positional_embeddings
+        self.segment_embeddings = segment_embeddings
+        self.embedding_layernorm = trained_layer_norm
+        self.embedding_dropout = nn.Dropout(p=0.1)
+
+
+        # Encoder blocks, 12 for BASE
+        self.es = []
+        # Create a list of weights per encoder, and then create an encoder based on them, i goes from 0 to 11
+
+        for i in range(0, self.L):
+            e = Encoder("base", batch_size, seq_len, layer_weights_dict[str(i)])
+            self.es.append(e)
+
+        # Input embeddings
+        self.input_embedding = nn.Embedding(512, 768)
+
+    def forward(self, X):
+        ## Tokenize the Input
+        batch = self.bert_tokenizer(X, max_length=10, truncation=True, padding="max_length") # normally max_length=512
+        batch_ids = torch.tensor(batch["input_ids"])
+        attention_mask = torch.tensor(batch["attention_mask"])
+        token_type_ids = torch.tensor(batch["token_type_ids"])
+        
+        ## Embeddings layers
+        token_embeddings = self.word_embeddings(batch_ids)
+        segment_embeddings = self.segment_embeddings(token_type_ids)
+        embeddings_with_position = self.positional_embeddings(token_embeddings.to(torch.int32) + segment_embeddings.to(torch.int32))
+        embedding_normalized = self.embedding_layernorm(embeddings_with_position)
+        final_embedding = self.embedding_dropout(embedding_normalized)
+
+        ## Apply encoders repeatedly to input.
+        output = self.es[0](final_embedding, attention_mask)
+        for e in self.es[1:]:
+            output = e(output, attention_mask)
+
+        # CONFIGURE OUTPUTS and OUTPUT LAYER
+        # Normalization, Sigmoid + output
+        return output
+    
+
+BERT_Model = BERT()
+out = BERT_Model("hello world")
+# print(out)
---
+++ bert/encoder.py
@@ -0,0 +1,107 @@
+import torch
+from torch import nn
+from math import sqrt
+
+class Encoder(nn.Module):
+
+    def __init__(self, model, batch_size, seq_len, pretrained_weights=0):
+        super().__init__()
+        self.pretrained_weights = pretrained_weights
+
+        # self.N is batch_size
+        self.N = batch_size
+        self.seq_len = seq_len # POLISH
+        # self.A is number of heads
+        self.A = None
+        # self.H is hidden size
+        self.H = None
+        # self.F is the size of the hidden layer in the feed-forward neural network
+        self.F = 3072
+
+        if model == "base":
+            self.A = 12
+            self.H = 768
+           
+        self.W = []
+        # Initialize weights for multi-head attention
+        if pretrained_weights == 0:
+            for i in range(0, self.A):
+                # d_model / h (or self.A) = 768 / 12 = 64 
+                WQ = torch.zeros((self.H, self.H // self.A))
+                WK = torch.zeros((self.H, self.H // self.A))
+                WV = torch.zeros((self.H, self.H // self.A))
+
+                nn.init.xavier_normal_(WQ)
+                nn.init.xavier_normal_(WK)
+                nn.init.xavier_normal_(WV)
+                self.W.append([WQ, WK, WV])
+
+
+            # dim(W_O) = h*d_v x d_model = 12*64 x 768 = 768 x 768
+            self.WO = torch.zeros((self.H, self.H))
+            nn.init.xavier_normal_(self.WO)
+
+            # define dropout, linear layers, layernorm, and GELU
+            self.linear1 = nn.Linear(self.H, self.F)
+            self.gelu = nn.GELU()
+            self.linear2 = nn.Linear(self.F, self.H)
+            self.dropout = nn.Dropout(0.1)
+            self.layernorm = nn.LayerNorm(self.H) # add normalized shape
+        else:
+            WQ = 
+
+            
+    def multi_head_self_attention(self, X: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        # the output for each head
+        Zs = []
+        for head_i in self.W:
+            WQ = head_i[0]
+            WK = head_i[1]
+            WV = head_i[2]
+            
+            # Q is (N x (self.H / self.A))
+            Q = X@WQ
+            K = X@WK
+            V = X@WV
+
+
+            scores = (Q@K.transpose(1, 2))/sqrt(self.H / self.A)
+            scores_masked = scores.masked_fill(mask.view(10, 1, 1) == 0, float('-inf'))
+            attn_weights = nn.functional.softmax(scores_masked, dim=-1)
+            
+            attn_weights = self.dropout(attn_weights)
+
+            Z = attn_weights@V
+            Zs.append(Z) 
+
+        # Concat attention output from each head.
+        O = torch.concat(Zs, dim=-1)@self.WO
+        return O
+    
+    def feed_forward_network(self, X: torch.Tensor) -> torch.Tensor:
+        X = self.linear1(X)
+        X = self.gelu(X)
+        X = self.linear2(X)
+        return X
+    
+    def forward(self, X: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        # X dimension is (N, self.H)
+        X1 = self.multi_head_self_attention(X, mask) # (N, self.H)
+        X = self.dropout(X1) + X
+        X = self.layernorm(X)
+        X2 = self.feed_forward_network(X) # (N, self.H)
+        X = self.dropout(X2) + X
+        X = self.layernorm(X)
+
+        return X
+    
+    def get(self):
+        return self.pretrained_weights
+
+"""
+Testing:
+encoder = Encoder("base", 2, 10)
+x = torch.randn(2, 10, 768)  # batch=2, seq_len=10
+mask = torch.zeros(2, 10, 768)
+out = encoder(x, mask)
+"""
